820476b903a4df4b00c6e337e3486a2f54eb2438
Tribunalov Alexandr <59662623+Ice-by@users.noreply.github.com>
2022-01-18 00:07:06 +0200

    feat: refresh main.js

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,1 +1,15 @@
-js
+// Smooth scrolling on anchors:
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+};
